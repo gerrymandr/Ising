5af5da12980cbb053af7bd4e0f0e49a6b51a9f71
Cara <nixxx179@umn.edu>
2018-06-25 15:20:54 -0400

    get adjacency matrix exported as a csv from a shape file. Yeah. We did that.

--- get_adj_matrix.py
+++ get_adj_matrix.py
@@ -3,17 +3,20 @@
 """
 Created on Mon Jun 25 11:12:54 2018
 
-@author: hannah
+@author(s): hannah & cara
 """
 import networkx as nx
 import pandas as pd
 import pickle as serializer
+
+
+#import pandas as pd 
 #from osgeo import ogr
 #import gdal
 #import graphviz as pgv
-G=nx.read_shp('/home/hannah/Desktop/2016 Santa Clara Blocks Buffered/SC_Blocks_Buffered_Adj.shp')
+G=nx.read_shp("/Users/caranix/miniconda3/envs/vrdi/vrdi_data/SC/SC_Blocks_Buffered_Adj.shp")
 A = nx.adjacency_matrix(G)
-Adj = A.todense()
-file = open('adj_mat_SC', 'w')
-file.write(Adj)   
-file.close() 
+df = pd.DataFrame(A.toarray())
+dd = pd.DataFrame(df)
+dd.to_csv("adj_matrix.csv")
+
